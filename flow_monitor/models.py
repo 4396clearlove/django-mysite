d4c3d4f90ca1522febe168238bc02a9e1c886fb6
runner56 <759775069@qq.com>
2017-01-02 23:41:38 +0800

    17.1.2 配合数据库调整，更新了views函数

--- flow_monitor/models.py
+++ flow_monitor/models.py
@@ -6,10 +6,12 @@ from django.db import models
 
 class ring_flow_table(models.Model):
     name = models.CharField(u"环路名称", max_length=50)
-    down_link_usage = models.FloatField(u"下行均值带宽利用率")
-    down_link_peak_usage = models.FloatField(u"下行峰值带宽利用率")
-    up_link_usage = models.FloatField(u"上行均值带宽利用率")
-    up_link_peak_usage = models.FloatField(u"上行峰值带宽利用率")
+    # down_link_usage = models.FloatField(u"下行均值带宽利用率")
+    # down_link_peak_usage = models.FloatField(u"下行峰值带宽利用率")
+    # up_link_usage = models.FloatField(u"上行均值带宽利用率")
+    # up_link_peak_usage = models.FloatField(u"上行峰值带宽利用率")
+    link_usage = models.FloatField(u"下行均值带宽利用率")
+    link_peak_usage = models.FloatField(u"下行峰值带宽利用率")
     date = models.DateField(u"日期")
 
     class Meta:
--- flow_monitor/views.py
+++ flow_monitor/views.py
@@ -12,18 +12,19 @@ def main(request):
 
 def get_top5(request):
     if request.method=='POST':
-        # date = datetime.datetime(2016, 12, 13)
-        date = ring_flow_table.objects.only('date').order_by('date')[:1][0].date
+        date = ring_flow_table.objects.only('date').order_by('-date')[:1][0].date
 
-        d_lt_u = When(down_link_usage__gt=F('up_link_usage'), then='down_link_usage')
-        condition = Case(d_lt_u, default='up_link_usage')
-        # import pdb;pdb.set_trace()
-        h_instances = ring_flow_table.objects.filter(Q(name__startswith='H'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
-        j_instances = ring_flow_table.objects.filter(Q(name__startswith='J'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
-        r_instances = ring_flow_table.objects.filter(Q(name__startswith='R'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
-        # h_instances = ring_flow_table.objects.filter(Q(name__startswith='H'),date=date).order_by("-down_link_usage")[:5]
-        # j_instances = ring_flow_table.objects.filter(Q(name__startswith='J'),date=date).order_by("-down_link_usage")[:5]
-        # r_instances = ring_flow_table.objects.filter(Q(name__startswith='R'),date=date).order_by("-down_link_usage")[:5]
+        # 使用了Case....When...来过滤，别删除
+        # d_lt_u = When(down_link_usage__gt=F('up_link_usage'), then='down_link_usage')
+        # condition = Case(d_lt_u, default='up_link_usage')
+        # h_instances = ring_flow_table.objects.filter(Q(name__startswith='H'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
+        # j_instances = ring_flow_table.objects.filter(Q(name__startswith='J'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
+        # r_instances = ring_flow_table.objects.filter(Q(name__startswith='R'),date=date).values('name').annotate(max_link_usage= condition).order_by('-max_link_usage').values_list('name','max_link_usage')[:5]
+        
+
+        h_instances = ring_flow_table.objects.filter(Q(name__startswith='H'),date=date).order_by("-link_usage").values_list('name','link_usage')[:5]
+        j_instances = ring_flow_table.objects.filter(Q(name__startswith='J'),date=date).order_by("-link_usage").values_list('name','link_usage')[:5]
+        r_instances = ring_flow_table.objects.filter(Q(name__startswith='R'),date=date).order_by("-link_usage").values_list('name','link_usage')[:5]
         return JsonResponse(
             {
                 "hring":{
@@ -46,17 +47,20 @@ def get_usage(request, id):
     if request.method=='POST':
         id = int(id)
         if id>=2 and id<=6:
-            # date = datetime.date(2016, 12, 13)
-            date = ring_flow_table.objects.only('date').order_by('date')[:1][0].date
-            ring = ring_flow_table.objects.filter(date=date).order_by("-down_link_usage")[:5][id-2].name
+            # import pdb;pdb.set_trace()
+            date = ring_flow_table.objects.only('date').order_by('-date')[:1][0].date
+            # ring = ring_flow_table.objects.filter(date=date).order_by("-down_link_usage")[:5][id-2].name
+            ring = ring_flow_table.objects.filter(date=date).order_by("-link_usage")[:5][id-2].name
             instances = ring_flow_table.objects.filter(name=ring).order_by("date")[:7]
         else:
             pass
 
         return JsonResponse(
             {
-            "averageUsage":[instance.down_link_usage if instance.down_link_usage>instance.up_link_usage else instance.up_link_usage  for instance in instances],
-            "peakUsage": [instance.down_link_peak_usage if instance.down_link_peak_usage>instance.up_link_peak_usage else instance.up_link_peak_usage  for instance in instances],
+            # "averageUsage":[instance.down_link_usage if instance.down_link_usage>instance.up_link_usage else instance.up_link_usage  for instance in instances],
+            # "peakUsage": [instance.down_link_peak_usage if instance.down_link_peak_usage>instance.up_link_peak_usage else instance.up_link_peak_usage  for instance in instances],
+            "averageUsage": [instance.link_usage for instance in instances],
+            "peakUsage": [instance.link_peak_usage for instance in instances],
             "ring": ring,
             "date":[instance.date for instance in instances]
             },
